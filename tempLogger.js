4363c6d8fbbbe3b4fd80b9a73fb4a76deee2bbc9
Sanaa Bhorkar <52894078+sbhorkar@users.noreply.github.com>
2023-06-21 21:19:24 -0700

    Create tempLogger.js

---
+++ tempLogger.js
@@ -0,0 +1,51 @@
+let intervalId;
+
+document.getElementById('logButton').addEventListener('click', function() {
+    var btn = this;
+    if(intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+        Puck.disconnect(function() {
+            btn.innerText = 'Start Logging';
+        });
+        return;
+    }
+
+    Puck.connect(function(err) {
+        if (err) {
+            console.log("Connection error: " + err);
+            return;
+        }
+
+        let code = `
+            let logInterval;
+            function startLogging() {
+                logInterval = setInterval(function() {
+                    let temp = E.getTemperature();
+                    let logEntry = "Temperature: " + temp.toFixed(2) + "°C at " + Date().toString() + "\\n";
+                    let currentData = require("Storage").read('tempLog.txt');
+                    require("Storage").write('tempLog.txt', currentData + logEntry);
+                }, 5 * 60 * 1000); // 5 minutes in milliseconds
+            }
+
+            function stopLogging() {
+                if(logInterval) {
+                    clearInterval(logInterval);
+                    logInterval = null;
+                }
+            }
+
+            startLogging();
+        `;
+
+        Puck.eval(code, function() {
+            intervalId = setInterval(function() {
+                Puck.eval("require('Storage').read('tempLog.txt')", function(data) {
+                    console.log(data);
+                });
+            }, 5 * 60 * 1000); // 5 minutes in milliseconds
+
+            btn.innerText = 'Stop Logging';
+        });
+    });
+});
